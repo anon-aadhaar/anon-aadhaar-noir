786717b18b2d37b2c121f0514174499d7255a623
0xvikasrushi <0xvikas@gmail.com>
2024-11-23 19:22:35 +0530

    feat: optimize extractor circuits

---
+++ circuits/get_gates.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+BB_CMD=${1:-"bb"}
+
+## Compile the circuit
+nargo compile --force --silence-warnings
+
+
+circuit_output=$("$BB_CMD" gates -b ./target/aadhaar_qr_verifier.json | grep "circuit_size")    
+circuit_size=$(echo "$circuit_output" | awk -F': ' '{print $2}' | tr -d ',')
+
+echo "circuit size: $circuit_size"
